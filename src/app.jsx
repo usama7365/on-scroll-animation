d83947e6336c49bc84b5abbc04366e25a1f6f0ff
usama aamir <usamaaamirsohail@gmail.com>
2021-12-22 15:42:21 +0500

    done

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Rout from "./routing"
+let App=()=>{
+    return(
+      <Rout/> 
+    )
+} 
+export default App
---
+++ src/routing.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Login from "./components/log";
+
+
+
+
+let Rout = () => {
+
+
+    return (
+        <div>
+            <Login />
+
+
+
+        </div>
+    )
+}
+export default Rout
+
+
+
